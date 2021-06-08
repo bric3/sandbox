cc2fba085fbc79f1f5f6884c47735400872b5e0e
Brice Dutheil <brice.dutheil@gmail.com>
2021-06-08 02:18:02 +0200

    Sparkline now supports downsampling

--- src/main/java/sandbox/Sparkline.java
+++ src/main/java/sandbox/Sparkline.java
@@ -24,40 +24,80 @@
 
 package sandbox;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.*;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+/**
+ * Library-less Sparkline util
+ */
 public class Sparkline {
   public static final Pattern DELIMITER = Pattern.compile(",|\\p{javaWhitespace}+");
   static PrintStream stdout = System.out;
+  static PrintStream stderr = System.err;
   static InputStream stdin = System.in;
 
   private static final char[] TICKS = {'▁', '▂', '▃', '▄', '▅', '▆', '▇', '█'};
 
-  public static void main(String... args) {
+  public static void main(String... args) throws IOException {
     double[] doubles = null;
 
-    if (args.length == 1) {
-      switch (args[0]) {
-        case "-":
-          try (var scanner = new Scanner(new BufferedInputStream(stdin), UTF_8)
-              .useDelimiter(DELIMITER)) {
-            final var builder = DoubleStream.builder();
-            while (scanner.hasNextDouble()) {
-              final var t = scanner.nextDouble();
-              builder.accept(t);
-            }
-            doubles = builder.build().toArray();
+    var argsDeque = new ArrayDeque<>(Arrays.asList(args));
+    var parsed = false;
+    InputStream stdin = null;
+    int samples = terminalColumns();
+
+    while (!parsed && !argsDeque.isEmpty()) {
+
+      String arg = argsDeque.pollFirst();
+      switch (arg) {
+        case "-f":
+          if (stdin != null) {
+            stderr.printf("The -f option can only be set once.%n%n");
+            help();
+            return;
           }
+          String fileOrStdin = argsDeque.pollFirst();
+          if (fileOrStdin == null) {
+            stderr.printf("The -f option requires an argument.%n%n");
+            help();
+            return;
+          }
+          if (Objects.equals("-", fileOrStdin)) {
+            stdin = Sparkline.stdin;
+          } else {
+            Path file = Path.of(fileOrStdin);
+            if (!Files.isReadable(file)) {
+              stderr.printf(file + " is not readable.%n%n");
+              help();
+              return;
+            }
+            stdin = Files.newInputStream(file);
+          }
+          break;
+
+        case "--no-downsample":
+          samples = -1;
+          break;
+        case "-d":
+        case "--downsample-to":
+          String size = argsDeque.pollFirst();
+          if (size == null) {
+            stderr.printf("Downsampling requires a positive integer.%n%n");
+            help();
+            return;
+          }
+          samples = Integer.parseInt(size);
           break;
         case "-h":
         case "--help":
@@ -66,16 +106,36 @@ public class Sparkline {
         case "--example-usage":
           example_usage();
           return;
+        default:
+          argsDeque.addFirst(arg);
+          parsed = true;
       }
     }
+
+    if (stdin != null) {
+      try (var scanner = new Scanner(new BufferedInputStream(stdin), UTF_8)
+          .useDelimiter(DELIMITER)) {
+        final var builder = DoubleStream.builder();
+        while (scanner.hasNextDouble()) {
+          final var t = scanner.nextDouble();
+          builder.accept(t);
+        }
+        doubles = builder.build().toArray();
+      }
+    }
+
     if (doubles == null) {
-      doubles = Arrays.stream(args)
+      doubles = argsDeque.stream()
           .flatMap(arg -> Arrays.stream(DELIMITER.split(arg)))
           .mapToDouble(Double::parseDouble)
           .toArray();
     }
 
     if (doubles.length != 0) {
+      if (samples != -1) {
+        doubles = largestTriangleThreeBuckets1D(doubles, samples);
+      }
+
       stdout.println(Sparkline.of(doubles));
     } else {
       help();
@@ -106,16 +166,47 @@ public class Sparkline {
   }
 
   private static void help() {
+    var sparkline = ProcessHandle.current().info().arguments().map(a -> a[0]).orElse("Sparkline");
+
     int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1};
-    stdout.println("java Sparkline " + Arrays.stream(intArray).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+    stdout.println("java " + sparkline + " " + Arrays.stream(intArray).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
     stdout.println(Sparkline.of(Arrays.stream(intArray).asDoubleStream().toArray()));
 
     double[] doubleArray = {1.5f, 0.5f, 3.5f, 2.5f, 5.5f, 4.5f, 7.5f, 6.5f};
-    stdout.println("java Sparkline " + Arrays.stream(doubleArray).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
+    stdout.println("java " + sparkline + " " + Arrays.stream(doubleArray).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
     stdout.println(Sparkline.of(doubleArray));
 
-    stdout.println("shuf -i 0-20 | java Sparkline -");
+    stdout.println("shuf -i 0-20 | java " + sparkline + " -f -");
     stdout.println(Sparkline.of(new Random().ints(20).asDoubleStream().toArray()));
+
+    stdout.println();
+    stdout.println();
+    stdout.println();
+
+    stdout.println(
+        """
+        Options
+            
+            -f,--file FILE
+            This option indicates the number will be taken from a file. To use stdin, use '-' as a value.
+            
+            --no-downsample
+            Toggle off downsampling of the data.
+            
+            -d,--downsample-to SAMPLES
+            Toggle on downsampling of the data with the number of SAMPLES.
+            
+            -h,--help
+            This message.
+            
+            --example-usage
+            Additional examples of use
+        """
+    );
+
+    stdout.println();
+    stdout.println();
+    stdout.println();
 
     stdout.println(
         """
@@ -169,4 +260,148 @@ public class Sparkline {
         );
     return line.toString();
   }
+
+  private static int terminalColumns() throws IOException {
+    var ttyConfig = getTtyConfig();
+
+    return Arrays.stream(new String[]{
+            "\\b([0-9]+)\\s+" + "columns" + "\\b",
+            "\\b" + "columns" + "\\s+([0-9]+)\\b",
+            "\\b" + "columns" + "\\s*=\\s*([0-9]+)\\b"
+        })
+        .map(pattern -> Pattern.compile(pattern).matcher(ttyConfig))
+        .filter(Matcher::find)
+        .findFirst()
+        .map(matcher -> Integer.parseInt(matcher.group(1)))
+        .orElse(-1);
+  }
+
+  private static CharSequence getTtyConfig() throws IOException {
+    // straw man check to identify if this is running in a terminal
+    // System.console() requires that both stdin and stdout are connected to a terminal
+    // which is not always the case (eg with pipes).
+    // However, it happens that trying to read from /dev/tty works
+    // when the application is connected to a terminal, and fails when not
+    // with the message '(Device not configured)'.
+    //
+    // Unfortunately Files::notExists or Files::isReadable don't fail.
+    //noinspection EmptyTryBlock
+    try (var ignored = Files.newInputStream(Path.of("/dev/tty"))) {
+    } catch (FileSystemException fileSystemException) {
+      return "";
+    }
+
+    Process p = new ProcessBuilder("stty", "-a")
+        .redirectInput(ProcessBuilder.Redirect.from(new File("/dev/tty")))
+        .start();
+
+    var output = new StringBuilder();
+    try (var stdout = p.getInputStream();
+         var stderr = p.getErrorStream();
+         var outReader = new BufferedReader(new InputStreamReader(stdout));
+         var errReader = new BufferedReader(new InputStreamReader(stderr));
+    ) {
+      String line;
+      while ((line = outReader.readLine()) != null) {
+        output.append(line).append("\n");
+      }
+      while ((line = errReader.readLine()) != null) {
+        output.append(line).append("\n");
+      }
+      p.waitFor();
+    } catch (InterruptedException e) {
+      throw new InterruptedIOException();
+    }
+    return output.toString();
+  }
+
+
+  /**
+   * Downsample a single dimension vector.
+   *
+   * <p>
+   * Uses a variation of the original <strong>Largest Triangle Three Bucket</strong>
+   * algorithm to perform the downsampling algorithm, instead it uses the position
+   * in the array as the abscissa.</p>
+   *
+   * <p>This variation naturally implies that all points are adjacent since a
+   * single dimension vector doesn't allow passing arbitrary abscissa.</p>
+   *
+   * @param data      A single dimensional vector, the point's <em>abscissa</em> (<em>x</em>)
+   *                  is the position in the vector, and the value is the <em>ordinate</em> (<em>y</em>).
+   *                  E.g. {@code new double[] { 1, -40, 1, 4, 6, 120, 10, 4, -3 } }
+   * @param threshold The number of data points to be returned. It needs to be greater than 2.
+   * @return The two-dimensional vector downsampled to <em>threshold</em> points.
+   */
+  public static double[] largestTriangleThreeBuckets1D(double[] data, int threshold) {
+    final int START_END_BUCKETS = 2;
+
+    Objects.requireNonNull(data);
+    int data_length = data.length;
+    if (data_length < 2 || threshold >= data_length) {
+      return data;
+    }
+    if (threshold <= START_END_BUCKETS) {
+      throw new IllegalArgumentException("threshold should be higher than 2");
+    }
+
+
+    double[] sampled = new double[threshold];
+    int sampleIndex = 0;
+
+    // bucket size.
+    double every = (data_length - START_END_BUCKETS) / (double) (threshold - START_END_BUCKETS);
+
+    int a = 0;  // Initially a is the first point in the triangle
+    double max_area_point = 0;
+    double max_area, area;
+    int next_a = 0;
+
+    sampled[sampleIndex++] = data[a]; // Always add the first point
+
+    for (int i = 0; i < threshold - START_END_BUCKETS; i++) {
+      // Calculate point average for next bucket (containing c)
+      double avg_x = 0, avg_y = 0;
+      int avg_range_start = (int) (floor((i + 1) * every) + 1),
+          avg_range_end = Math.min((int) (floor((i + 2) * every) + 1), data_length);
+
+      double avg_range_length = avg_range_end - avg_range_start;
+
+      for (; avg_range_start < avg_range_end; avg_range_start++) {
+        avg_x += avg_range_start;
+        avg_y += data[avg_range_start];
+      }
+      avg_x /= avg_range_length;
+      avg_y /= avg_range_length;
+
+      // Get the range for this bucket
+      int range_offs = (int) (floor((i) * every) + 1),
+          range_to = (int) (floor((i + 1) * every) + 1);
+
+      // Point a
+      double point_a_x = a, point_a_y = data[a];
+
+      max_area = -1;
+
+      for (; range_offs < range_to; range_offs++) {
+        // Calculate triangle area over three buckets
+        area = abs(
+            (point_a_x - avg_x) * (data[range_offs] - point_a_y) - (point_a_x - range_offs) * (avg_y - point_a_y)
+        ) * 0.5;
+        if (area > max_area) {
+          max_area = area;
+          max_area_point = data[range_offs];
+          next_a = range_offs; // Next a is this b
+        }
+      }
+
+      sampled[sampleIndex++] = max_area_point; // Pick max area point from the bucket
+      a = next_a;
+
+    }
+    sampled[sampleIndex] = data[data_length - 1]; // Always add last point
+
+    return sampled;
+  }
+
 }
--- src/test/java/sandbox/SparklineTest.java
+++ src/test/java/sandbox/SparklineTest.java
@@ -30,7 +30,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.security.SecureRandom;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,23 +44,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SparklineTest {
 
   private ByteArrayOutputStream stdout;
+  private ByteArrayOutputStream stderr;
 
 
   @BeforeEach
   void set_stdout() {
     stdout = new ByteArrayOutputStream();
     Sparkline.stdout = new PrintStream(stdout, true, UTF_8);
+    stderr = new ByteArrayOutputStream();
+    Sparkline.stderr = new PrintStream(stderr, true, UTF_8);
   }
 
   @AfterEach
   void restore_stdout() {
     Sparkline.stdout.close();
+    Sparkline.stderr.close();
     Sparkline.stdin = System.in;
     Sparkline.stdout = System.out;
+    Sparkline.stderr = System.err;
   }
 
   @Test
-  void it_shows_help_with_no_argv() {
+  void it_shows_help_with_no_argv() throws IOException {
     Sparkline.main();
     assertThat(stdout.toString(UTF_8))
         .contains("java Sparkline 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1")
@@ -67,7 +75,7 @@ class SparklineTest {
   }
 
   @Test
-  void it_shows_help_with_short_help_option() {
+  void it_shows_help_with_short_help_option() throws IOException {
     Sparkline.main("-h");
     assertThat(stdout.toString(UTF_8))
         .contains("java Sparkline 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1")
@@ -77,7 +85,7 @@ class SparklineTest {
   }
 
   @Test
-  void it_shows_help_with_long_help_option() {
+  void it_shows_help_with_long_help_option() throws IOException {
     Sparkline.main("--help");
     assertThat(stdout.toString(UTF_8))
         .contains("java Sparkline 1 2 3 4 5 6 7 8 7 6 5 4 3 2 1")
@@ -87,7 +95,7 @@ class SparklineTest {
   }
 
   @Test
-  void it_shows_examples_with_long_example_usage_option() {
+  void it_shows_examples_with_long_example_usage_option() throws IOException {
     Sparkline.main("--example-usage");
     assertThat(stdout.toString(UTF_8))
         .contains("# Magnitude of earthquakes worldwide 2.5 and above in the last 24 hours")
@@ -95,47 +103,102 @@ class SparklineTest {
   }
 
   @Test
-  void it_charts_pipe_data_delimited_by_space() {
+  void report_missing_value_for_file_option() throws IOException {
+    Sparkline.main("-f");
+    assertThat(stderr.toString(UTF_8))
+        .contains("The -f option requires an argument.");
+  }
+
+  @Test
+  void report_too_many_file_option() throws IOException {
+    Sparkline.main("-f", "-", "-f", "file");
+    assertThat(stderr.toString(UTF_8))
+        .contains("The -f option can only be set once.");
+  }
+
+  @Test
+  void report_unreadable_file_option() throws IOException {
+    Sparkline.main("-f", "not_there");
+    assertThat(stderr.toString(UTF_8))
+        .contains("not_there is not readable");
+  }
+
+  @Test
+  void report_missing_sample_value_option() throws IOException {
+    Sparkline.main("-d");
+    assertThat(stderr.toString(UTF_8))
+        .contains("Downsampling requires a positive integer.");
+  }
+
+  @Test
+  void it_charts_pipe_data_delimited_by_space() throws IOException {
     Sparkline.stdin = new ByteArrayInputStream("0 30 55 80 33 150\n".getBytes(UTF_8));
-    Sparkline.main("-");
+    Sparkline.main("-f", "-");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁▂▄▅▃█");
   }
 
   @Test
-  void it_charts_pipe_data_delimited_by_comma() {
+  void it_charts_pipe_data_delimited_by_comma() throws IOException {
     Sparkline.stdin = new ByteArrayInputStream("0,30,55,80,33,150".getBytes(UTF_8));
-    Sparkline.main("-");
+    Sparkline.main("-f", "-");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁▂▄▅▃█");
   }
 
   @Test
-  void it_charts_args_space_delimited_data() {
+  void it_charts_args_space_delimited_data() throws IOException {
     Sparkline.main("0 30 55 80 33 150");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁▂▄▅▃█");
   }
 
   @Test
-  void it_charts_args_comma_delimited_data() {
+  void it_charts_args_comma_delimited_data() throws IOException {
     Sparkline.main("0,30,55,80,33,150");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁▂▄▅▃█");
   }
 
   @Test
-  void it_charts_args_mixed_space_and_comma_delimited_data() {
+  void it_charts_args_mixed_space_and_comma_delimited_data() throws IOException {
     Sparkline.main("0,30,55,80", "33,150", "20,100,2.1 33.4");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁▂▄▅▃█▂▆▁▃");
   }
 
   @Test
-  void it_handles_decimals() {
+  void it_handles_decimals() throws IOException {
     Sparkline.main("5.5", "20");
 
     assertThat(stdout.toString(UTF_8)).isEqualToIgnoringNewLines("▁█");
+  }
+
+  @Test
+  void it_can_downsample() throws IOException {
+    Sparkline.main(Stream.concat(
+        Stream.of("-d", "100"),
+        new SecureRandom().doubles(1000).mapToObj(String::valueOf)
+    ).toArray(String[]::new));
+
+    assertThat(stdout.toString(UTF_8).trim().codePoints().count()).isEqualTo(100);
+  }
+
+  @Test
+  void it_does_not_downsample_if_not_in_a_terminal_by_default() throws IOException {
+    Sparkline.main(new SecureRandom().doubles(1000).mapToObj(String::valueOf).toArray(String[]::new));
+
+    assertThat(stdout.toString(UTF_8).trim().codePoints().count()).isEqualTo(1000);
+  }
+
+  @Test
+  void it_does_not_downsample_if_disabled() throws IOException {
+    Sparkline.main(Stream.concat(
+        Stream.of("--no-downsample"),
+        new SecureRandom().doubles(1000).mapToObj(String::valueOf)
+    ).toArray(String[]::new));
+
+    assertThat(stdout.toString(UTF_8).trim().codePoints().count()).isEqualTo(1000);
   }
 
   @Test
