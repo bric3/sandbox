16bebd0dc483a7e55561767b93b9edd6743a8e72
Brice Dutheil <brice.dutheil@gmail.com>
2022-04-04 14:18:25 +0200

    build: Bump dependency-updates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("com.github.ben-manes.versions") version ("0.39.0")
+  id("com.github.ben-manes.versions") version ("0.42.0")
 }
 
 val jmhProjects = listOf(project(":jmh-stuff"), project(":jmh-panama"))
