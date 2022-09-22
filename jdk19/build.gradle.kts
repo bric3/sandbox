41dc71d5c1dc7c8534a4defdad8d2263b68fd08f
Brice Dutheil <brice.dutheil@gmail.com>
2022-09-22 10:55:49 +0200

    chore: Fixed license header

--- jdk19/build.gradle.kts
+++ jdk19/build.gradle.kts
@@ -1,7 +1,7 @@
 /*
  * sandbox
  *
- * Copyright (c) ${year} - Brice Dutheil <brice.dutheil@gmail.com>
+ * Copyright (c) 2021,today - Brice Dutheil <brice.dutheil@gmail.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
