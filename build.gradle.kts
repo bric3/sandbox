e1943dafc24853997ea2250ff07fa224896ebc31
Brice Dutheil <brice.dutheil@gmail.com>
2023-07-03 12:20:22 +0200

    chore: Try configure copyright for IntelliJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+import org.jetbrains.gradle.ext.copyright
+import org.jetbrains.gradle.ext.settings
+
+plugins {
+  alias(libs.plugins.ideaExt)
+}
+
 val jmhProjects = setOf(
   project(":jmh-stuff"),
   project(":jmh-panama")
@@ -25,5 +32,41 @@ configure(javaProjects) {
 
   repositories {
     mavenCentral()
+  }
+}
+
+idea {
+  project.settings {
+    withIDEADir {
+      val scope = resolve("scopes")
+      scope.mkdirs()
+
+      val file = scope.resolve("sandbox.xml")
+      file.writeText("<component/>")
+
+      withIDEAFileXml(file.toRelativeString(this)) {
+        val root = this.asNode()
+        root.attributes()["name"] = "DependencyValidationManager"
+        root.appendNode(
+          "scope", mapOf(
+            "name" to rootProject.name,
+            "pattern" to "file[${rootProject.name}]:*/"
+          )
+        )
+      }
+    }
+
+    copyright {
+      val mpl2Copyright = profiles.create("MPL2") {
+        keyword = "MPL2"
+        notice = rootProject.file("HEADER").readLines().joinToString("\n") { line ->
+          line.replace("\${year}", "2021, today")
+            .replace("\${name}", "Brice Dutheil")
+        }
+      }
+      scopes = mapOf(
+        rootProject.name to mpl2Copyright.name
+      )
+    }
   }
 }
