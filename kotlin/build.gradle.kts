0c1afc2c941f279d3fcc1466eeb633a41031c499
Brice Dutheil <brice.dutheil@gmail.com>
2025-02-08 12:26:49 +0100

    chore(deps): Mockito for kotlin sandbox
    # Conflicts:
    #	kotlin/build.gradle.kts

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -9,31 +9,35 @@
  */
 
 plugins {
-    id("sandbox.kotlin-conventions")
+  id("sandbox.kotlin-conventions")
 }
 
 repositories {
-    mavenCentral()
-    maven("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
+  mavenCentral()
+  maven("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
 }
 
 dependencies {
-    implementation(kotlin("stdlib", libs.versions.kotlin.get()))
-    implementation(kotlin("reflect", libs.versions.kotlin.get()))
+  implementation(kotlin("stdlib", libs.versions.kotlin.get()))
+  implementation(kotlin("reflect", libs.versions.kotlin.get()))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${libs.versions.kotlinCoroutines.get()}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${libs.versions.kotlinCoroutines.get()}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${libs.versions.kotlinCoroutines.get()}")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${libs.versions.kotlinCoroutines.get()}")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${libs.versions.kotlinCoroutines.get()}")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${libs.versions.kotlinCoroutines.get()}")
 
-    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.10.0")
-    implementation("org.jetbrains.lets-plot:lets-plot-batik:4.6.2")
-    implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.8.0")
-    implementation("org.jetbrains.kotlinx:kandy-util:0.8.0")
-    // currently on a private repository, see https://github.com/Kotlin/kandy/issues/284
-    implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.4.0")
+  implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.10.0")
+  implementation("org.jetbrains.lets-plot:lets-plot-batik:4.6.2")
+  implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.8.0")
+  implementation("org.jetbrains.kotlinx:kandy-util:0.8.0")
+  // currently on a private repository, see https://github.com/Kotlin/kandy/issues/284
+  implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.4.0")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${libs.versions.kotlinCoroutines.get()}")
-    testImplementation(platform("org.junit:junit-bom:${libs.versions.junit.jupiter.get()}"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("app.cash.turbine:turbine:1.2.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${libs.versions.kotlinCoroutines.get()}")
+  testImplementation(platform("org.junit:junit-bom:${libs.versions.junit.jupiter.get()}"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation("app.cash.turbine:turbine:1.2.0")
+
+  testImplementation(libs.mockito.core)
+  testImplementation(libs.mockito.jupiter)
+  testImplementation(libs.mockito.kotlin)
 }
